8002c1ef78d076735cbdbd82c78399286f8c9e07
Zalaltor <murtazadooz6@gmail.com>
2026-07-06 17:13:25 +0500

    Delete tailwind.config.ts

--- tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: { extend: {} },
-  plugins: [require("@tailwindcss/typography")],
-};
-
-export default config;
